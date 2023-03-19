34014c9abe871500ed978cc663d4671d85f27154
Zigatase <zigatase@gmail.com>
2023-03-19 14:16:38 +0300

    0.2.0

--- exemples/bitcoin_lib/creat_wallet_btc.py
+++ exemples/bitcoin_lib/creat_wallet_btc.py
@@ -2,15 +2,15 @@ from safecryptoapi.bitcoinlib.wallets import Wallet
 
 # --- Creat BTC Address ---
 # Creat Legacy (P2PKH) Address
-wallet_P2PKH = Wallet.create('WalletLegacy1', network='bitcoin', witness_type='legacy')
+wallet_P2PKH = Wallet.create('WalletLegacy', network='bitcoin', witness_type='legacy')
 key_P2PKH = wallet_P2PKH.get_key()
 
 # Creat Segwit (P2WPKH) Address
-wallet_P2WPKH = Wallet.create('WalletSegwit1', network='bitcoin', witness_type='segwit')
+wallet_P2WPKH = Wallet.create('WalletSegwit', network='bitcoin', witness_type='segwit')
 key_P2WPKH = wallet_P2WPKH.get_key()
 
 # Creat Script (P2SH)
-wallet_P2SH = Wallet.create('WalletScript1', network='bitcoin', witness_type='p2sh-segwit')
+wallet_P2SH = Wallet.create('WalletScript', network='bitcoin', witness_type='p2sh-segwit')
 key_P2SH = wallet_P2SH.get_key()
 
 # Print For All Address
